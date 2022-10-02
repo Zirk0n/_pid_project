8d03cec71b1b41995c5df095ad64d02b2352933a
Zirk0n <bohacekmichael@gmail.com>
2022-10-02 20:04:11 +0200

    uprava

--- pid/urls.py
+++ pid/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import ListProdejniMisto
+from .views import ListProdejniMisto, DetailProdejniMisto
 
 app_name = "pid"
 
 urlpatterns = [
     path("prodejni-mista/", ListProdejniMisto.as_view(), name="ListProdejniMisto"),
-    path("prodejni-mista/<slug:slug>",views., name=""),
+    path("prodejni-mista/<slug:slug>", DetailProdejniMisto.as_view(), name="DetailProdejniMisto"),
 ]
